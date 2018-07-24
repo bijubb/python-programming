fe841bcfd76d00c6cc8ebc5adf61bcd2fdc208c2
bijubb <36730824+bijubb@users.noreply.github.com>
2018-07-24 16:00:31 +0530

    Create primen between intervals.py

---
+++ primen between intervals.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the start limit number:"))
+b=int(input("Enter the end limit number:"))
+for num in range(a,b+1):
+  if num>1:
+    for i in range(2,num):
+      break:
+   else:
+      print(num)
